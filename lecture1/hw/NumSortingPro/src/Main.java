c78125d0d8f0eb9ff722182af64aed8aaa393eaa
AndrewIvlev <mr.andrew.ivlev@gmail.com>
2019-02-05 22:25:44 +0300

    Quick sotring added, test in main class created

--- lecture1/hw/NumSortingPro/src/Main.java
+++ lecture1/hw/NumSortingPro/src/Main.java
@@ -1,4 +1,22 @@
+import java.util.Arrays;
+import java.util.Random;
+
+import SortPackage.QuickSort;
+
 public class Main {
     public static void main(String[] args) {
+        int n = 100;
+        int MaxRand = 1000;
+        Random random = new Random();
+        int array[] =  new int[n];
+
+
+        for (int i = 0; i < n; i++){
+            array[i] = random.nextInt(MaxRand);
+        }
+
+        QuickSort.quickSort(array);
+
+        System.out.println(Arrays.toString(array));
     }
 }
---
+++ lecture1/hw/NumSortingPro/src/SortPackage/QuickSort.java
@@ -0,0 +1,35 @@
+package SortPackage;
+
+public class QuickSort {
+    public static void quickSort(int array[]) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static void quickSort(int array[], int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int p = lomutoPartition(array, left, right);
+        quickSort(array, left, p - 1);
+        quickSort(array, p + 1, right);
+    }
+
+    private static int lomutoPartition(int array[], int left, int right) {
+        int pivot = array[right];
+        int i = left - 1;
+        for (int j = left; j < right; j++) {
+            if (array[j] < pivot) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+        swap(array, i + 1, right);
+        return i + 1;
+    }
+
+    private static void swap(int array[], int a, int b) {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+}
