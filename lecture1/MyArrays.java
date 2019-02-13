332d81fc61429db931be1b7fd77509b544ee32bc
dorofeevant <47380668+dorofeevant@users.noreply.github.com>
2019-02-13 15:33:00 +0300

    добавлены исходные файлы 'массив'

---
+++ lecture1/MyArrays.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class MyArrays {
+    int [] array = new int[3];
+    Candy[] box = new Candy[5];
+
+
+    int[] array2 = {10,3,7};
+    Candy CandyFirst = new Candy();
+    Candy CandySecond = new Candy();
+    Candy CandyThird = new Candy();
+
+    Candy [] box2 = {CandyFirst,CandySecond,CandyThird};
+    Candy [] bax3;
+
+
+    public void foo(){
+        /*
+        Candy Candy = new Candy();
+        box[0] = Candy;
+        System.out.println(box[0]);//вывод обекта
+*/
+
+       /* for (int i=0; i<array2.length; i++){
+            System.out.println(array2 (i));  // пробигаем по всему массиву
+        }
+        */
+    }
+
+}
