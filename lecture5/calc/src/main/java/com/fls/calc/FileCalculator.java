a9ca8a1df65bf54d68537dfcb9f1a2da4bffcef5
Kiselev Denis <denis.kiselev.1997@gmail.com>
2019-03-04 17:28:04 +0300

    Added comments

--- lecture5/calc/src/main/java/com/fls/calc/FileCalculator.java
+++ lecture5/calc/src/main/java/com/fls/calc/FileCalculator.java
@@ -5,6 +5,7 @@ import java.io.*;
 public class FileCalculator {
 
     public static double calculate(String pathName) throws Exception {
+        // Check file extension
         String extension;
         try {
             extension = pathName.substring(pathName.length() - 4);
@@ -16,15 +17,18 @@ public class FileCalculator {
             throw new IncorrectFileExtensionException("Only .txt extension supported");
         }
 
-        File file = new File(pathName);
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+        // If the file does not exist, then an exception will
+        // appear that will not be processed and will go higher
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(pathName)));
 
         char operation;
         double[] arguments;
-        
+
         try {
             String line = br.readLine();
 
+            // If the first line of the file is not one
+            // character, then we throw an exception
             if (line.length() != 1) {
                 throw new Exception();
             }
@@ -34,10 +38,14 @@ public class FileCalculator {
             line = br.readLine();
             String[] args = line.split(" ");
             arguments = new double[args.length];
+
+            // If in the second line of the file there is something
+            // other than numbers, then an exception is thrown
             for (int i = 0; i < args.length; i++) {
                 arguments[i] = Double.parseDouble(args[i]);
             }
 
+            // If not the end of the file, then throw an exception
             if (br.readLine() != null) {
                 throw new Exception();
             }
